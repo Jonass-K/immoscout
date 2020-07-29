3e6353c96ddc8037b0c4a524f75eb2e3788e424e
Jonass-K <60469464+Jonass-K@users.noreply.github.com>
2020-07-29 20:50:18 +0200

    Bugfixes and improve memory usage

--- immoscout.py
+++ immoscout.py
@@ -11,77 +11,90 @@ from email.mime.application import MIMEApplication
 from bs4 import BeautifulSoup
 from getpass import getpass
 
-
-def checkInput():
-    states = ['baden-wuerttemberg', 'bayern', 'berlin', 'brandenburg', 'bremen', 'hamburg', 'hessen', 'mecklenburg-vorpommern',
-        'niedersachsen', 'nordrhein-westfalen', 'rheinland-pfalz', 'saarland', 'sachsen', 'sachsen-anhalt', 'schleswig-holstein', 'thueringen']
-    if sys.argv[1] not in states:
-        print(usage)
-        sys.exit()
-
 my_mail = ''
 to_mail = ''
+smtp_host = ''
+smtp_port = 0
 msg = MIMEMultipart()
 msg['From'] = my_mail
 msg['To'] = to_mail
 usage = 'usage: immoscout.py <state> <city> <room> <price>'
-url = "https://www.immobilienscout24.de/Suche/de/" + 
-    state + "/" + 
-    city + "/" + 
-    room + "-zimmer-wohnung-mieten?price=-" +
-    price + ".0&enteredFrom=result_list#/"
 
-checkInput()
+if len(sys.argv) != 5:
+    print(usage)
+    sys.exit()
+
 state = sys.argv[1]
 city = sys.argv[2]
 room = sys.argv[3]
 price = sys.argv[4]
+url = 'https://www.immobilienscout24.de/Suche/de/' + state + '/' + city + '/' + \
+    room + '-zimmer-wohnung-mieten?price=-' + \
+    price + '.0&enteredFrom=result_list#/'
 my_password = getpass()
 
+k = 0
+
 while True:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    results = soup.find_all('a', 'result-list-entry__brand-title-container')
-    old_apartements = new_apartements
+    old_apartements = []
     new_apartements = []
-    new_apartements.append(['Title', 'Id', 'Link', 'Old/New'])
     i = 0
     apartement_found = False
 
+    # sleep for 8 hours
+    if k != 0:
+        time.sleep(60*60*8)
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    results = soup.find_all('a', 'result-list-entry__brand-title-container')
+    new_apartements.append(['Title', 'Id', 'Link', 'Old/New'])
+
     for result in results:
         if (str(result.h5.span) == 'None'):
-            new_apartement = [result.h5.string, result['href'].replace('/expose/', ''), 'https://www.immobilienscout24.de' + result['href'], 'Old']
+            new_apartement = [result.h5.string, result['href'].replace(
+                '/expose/', ''), 'https://www.immobilienscout24.de' + result['href'], 'Old']
         else:
-            new_apartement = [str(result.h5).rsplit('</span>')[1].replace('</h5>', ''), result['href'].replace('/expose/', ''), 'https://www.immobilienscout24.de' + result['href'], 'Old']
+            new_apartement = [str(result.h5).rsplit('</span>')[1].replace('</h5>', ''), result['href'].replace(
+                '/expose/', ''), 'https://www.immobilienscout24.de' + result['href'], 'Old']
         new_apartements.append(new_apartement)
-        print(result.h5.span)
 
-    for apartement in new_apartements:
-        if apartement not in old_apartements:
-            i += 1
-            apartement[3] = 'New'
-            apartement_found = True
+    with open('./apartements.csv', 'w+', newline='') as my_file:
+        if k == 0:
+            for apartement in new_apartements:
+                i += 1
+                apartement[3] = 'New'
+                apartement_found = True
+        else:
+            reader = csv.reader(my_file)
+            for apartement in new_apartements:
+                for old_apartement in old_apartements:
+                    if apartement[1] == old_apartement[1]:
+                        i += 1
+                        apartement[3] = 'New'
+                        apartement_found = True
 
-    with open('./apartements.csv', 'w', newline='') as my_file:
         writer = csv.writer(my_file, quoting=csv.QUOTE_ALL)
         writer.writerows(new_apartements)
+
+    with open('./apartements.csv', 'r', newline='') as my_file:
         part = MIMEApplication(my_file.read(), Name='apartements.csv')
-        
-    part['Content-Disposition'] = 'attachment; filename="%s"' % 'apartements.csv'        
+
+    part['Content-Disposition'] = 'attachment; filename="%s"' % 'apartements.csv'
     msg.attach(part)
 
     if apartement_found is True:
-        if i is 1:
-            msg['Subject'] = i + ' neues Apartement wurde gefunden.'
+        if i == 2:
+            msg['Subject'] = str(i - 1) + ' neues Apartement wurde gefunden.'
         else:
-            msg['Subject'] = i + ' neue Apartements wurden gefunden.'     
+            msg['Subject'] = str(i - 1) + ' neue Apartements wurden gefunden.'
         emailText = 'Im Anhang befindet sich eine CSV Datei mit allen Apartements.'
         msg.attach(MIMEText(emailText, 'html'))
-        
-        server = smtplib.SMTP('', 587)
+
+        server = smtplib.SMTP(smtp_host, smtp_port)
         server.starttls()
         server.login(my_mail, my_password)
         text = msg.as_string()
         server.sendmail(my_mail, to_mail, text)
         server.quit()
-    time.sleep(8*60*60)                
+    k += 1
